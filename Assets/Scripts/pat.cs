33f772de8c84cb59439d41d1bb34313a60516719
Gleb333-3 <glebefremenko23@gmail.com>
2021-03-16 22:05:37 +0300

    commit

---
+++ Assets/Scripts/pat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pat : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _yd;
+
+    [SerializeField]
+    private GameObject _ydJo;
+
+    [SerializeField]
+    private Transform _Patron;
+
+
+    [SerializeField]
+    private Transform _Ne1;
+
+    [SerializeField]
+    private Transform _DisPatr;
+
+    [SerializeField]
+    private Transform _DisJon;
+
+    [SerializeField]
+    private Transform _Jo1;
+
+    private void Update()
+    {
+        var dis = Vector3.Distance(_Ne1.position, _Jo1.position);
+        var dir = Vector3.Distance(_DisPatr.position, _DisJon.position);
+        if (dis < 4)
+        {
+            _Patron.position = Vector3.MoveTowards(_Patron.position, transform.position, 10 * Time.deltaTime);
+
+        }
+
+        if (dir < 1)
+        {
+            Destroy(_yd);
+            _ydJo.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/загадка.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class загадка : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _Plit;
+
+
+    [SerializeField]
+    private Transform _Joo;
+
+
+    [SerializeField]
+    private GameObject _Cten;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var di = Vector3.Distance(_Plit.position, _Joo.position);
+        if(di < 0.4)
+        {
+            Destroy(_Cten);
+        }
+    }
+}
